ce09ab49a40852d31cc158936aac94d4c9236a8d
Andreas Zhang <denglitsch@gmail.com>
2024-08-02 20:33:50 +0800

    update: insert magnet link into html

--- save_page_tools/query_magnet_sample-img.py
+++ save_page_tools/query_magnet_sample-img.py
@@ -13,6 +13,9 @@ from bs4 import BeautifulSoup
 
 proxy_addr = "127.0.0.1:8118"
 
+html_global = ''
+complete_name_globals = ''
+
 def getAjax(avid):
     '''获取javbus的ajax'''
 
@@ -39,7 +42,8 @@ def getAjax(avid):
     ]
     urllib.request.install_opener(opener)
     soup = BeautifulSoup(urllib.request.urlopen(url).read().decode('utf-8'), 'lxml')
-    html=soup.prettify()
+    html = soup.prettify()
+    html = html.replace("<div id=\"movie-loading\">","<div id=\"movie-loading\" style=\"display: none;\">")
     print(html)
     # save_path = os.path.expanduser('~/Downloads/')
     save_path = os.getcwd()
@@ -48,8 +52,14 @@ def getAjax(avid):
     file_name = title + '.html'
     # print(file_name)
     complete_name = os.path.join(save_path, file_name)
-    file_object = codecs.open(complete_name, "w", "utf-8")
-    file_object.write(html)
+
+    global html_global
+    global complete_name_globals
+    html_global = html
+    complete_name_globals = complete_name
+    
+    # file_object = codecs.open(complete_name, "w", "utf-8")
+    # file_object.write(html)
 
     '''获取img'''
     img_pattern = re.compile(r"var img = '.*?'")
@@ -108,12 +118,21 @@ def javbus(avid):
     soup = BeautifulSoup(urllib.request.urlopen(url).read().decode('utf-8'), 'lxml')
 
     html=soup.prettify()
-    title='ajax'
-    save_path = os.getcwd()
-    file_name = title + '.html'
-    complete_name = os.path.join(save_path, file_name)
-    file_object = codecs.open(complete_name, "w", "utf-8")
-    file_object.write(html)
+    # title='ajax'
+    # save_path = os.getcwd()
+    # file_name = title + '.html'
+    # complete_name = os.path.join(save_path, file_name)
+    # file_object = codecs.open(complete_name, "w", "utf-8")
+    # file_object.write(html)
+    html = "\n".join(html.split('\n')[2:])
+    html = "\n".join(html.split('\n')[:-3])
+    html = html + '\n'
+    html = html + "    </table>\n    <div id=\"movie-loading\" style=\"display: none;\">\n"
+    global html_global
+    global complete_name_globals
+    html_global = html_global.replace("    </table>\n    <div id=\"movie-loading\" style=\"display: none;\">\n", html)
+    file_object = codecs.open(complete_name_globals, "w", "utf-8")
+    file_object.write(html_global)
 
     avdist={'title':'','magnet':'','size':'','date':''}
 
@@ -132,46 +151,46 @@ def javbus(avid):
                 avdist['date'] = td.a.text.replace(" ", "").replace("\t", "").replace("\r\n","")
         print(avdist)
 
-url="https://www.javbus.com/ja/BNST-036"
-#有些网站会现在，但可伪装浏览器爬取 浏览器User-Agent的详细信息(可采用下面的进行爬虫伪装) 
-#浏览器头信息代理可以直接搜Http Header之User-Agent，以下是谷歌浏览器的
-headers = {
-        'accept-language': 'zh-CN,zh;q=0.9,zh-HK;q=0.8',
-        'cache-control': 'max-age=0',
-        'sec-ch-ua': '"Chromium";v="117", "Not;A=Brand";v="8"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'none',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'Accept': 'application/json',
-    };
+# url="https://www.javbus.com/ja/BNST-036"
+# #有些网站会现在，但可伪装浏览器爬取 浏览器User-Agent的详细信息(可采用下面的进行爬虫伪装) 
+# #浏览器头信息代理可以直接搜Http Header之User-Agent，以下是谷歌浏览器的
+# headers = {
+#         'accept-language': 'zh-CN,zh;q=0.9,zh-HK;q=0.8',
+#         'cache-control': 'max-age=0',
+#         'sec-ch-ua': '"Chromium";v="117", "Not;A=Brand";v="8"',
+#         'sec-ch-ua-mobile': '?0',
+#         'sec-ch-ua-platform': '"macOS"',
+#         'sec-fetch-dest': 'document',
+#         'sec-fetch-mode': 'navigate',
+#         'sec-fetch-site': 'none',
+#         'sec-fetch-user': '?1',
+#         'upgrade-insecure-requests': '1',
+#         'Accept': 'application/json',
+#     };
 
 
-# #使用requests方法
-def use_requests(url,headers):
-    #实践发现，request不用headers也可以爬到设了防爬限制的网站
-    #response = requests.get(url)
-    # 使用伪装浏览器的urllib方法
-    response = requests.get(url,headers=headers)
+# # #使用requests方法
+# def use_requests(url,headers):
+#     #实践发现，request不用headers也可以爬到设了防爬限制的网站
+#     #response = requests.get(url)
+#     # 使用伪装浏览器的urllib方法
+#     response = requests.get(url,headers=headers)
 
-    # 检查请求是否成功
-    if response.status_code == 200:
-        # 解析JSON数据
-        data1 = response.json()
-        print(data1)
-    else:
-        print('请求失败')
+#     # 检查请求是否成功
+#     if response.status_code == 200:
+#         # 解析JSON数据
+#         data1 = response.json()
+#         print(data1)
+#     else:
+#         print('请求失败')
 
-    data = response.text
-    #print(data)
-    #file_path="E:/Python/bilibili/bilibili.html"
-    file_path="bilibili.html"
-    #将爬到的内容保存到本地
-    with open(file_path,"w",encoding="utf-8") as f:
-        f.write(data)
+#     data = response.text
+#     #print(data)
+#     #file_path="E:/Python/bilibili/bilibili.html"
+#     file_path="bilibili.html"
+#     #将爬到的内容保存到本地
+#     with open(file_path,"w",encoding="utf-8") as f:
+#         f.write(data)
 
 
 if __name__ == '__main__':
